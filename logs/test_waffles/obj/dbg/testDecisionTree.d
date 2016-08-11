f266279279be81ebd49c0ef8f416b06ee52b8014
vickyjm <mvjorgemauriello@gmail.com>
2016-08-10 22:44:50 -0400

    Decision Tree example

---
+++ logs/test_waffles/obj/dbg/testDecisionTree.d
@@ -0,0 +1,2 @@
+../obj/dbg/testDecisionTree.o: testDecisionTree.cpp
+	g++ -Wall -Werror -Wshadow -pedantic -std=c++11 -I/usr/local/include -I../../../src -g -D_DEBUG -c testDecisionTree.cpp -o ../obj/dbg/testDecisionTree.o
---
+++ logs/test_waffles/obj/opt/testDecisionTree.d
@@ -0,0 +1,2 @@
+../obj/opt/testDecisionTree.o: testDecisionTree.cpp
+	g++ -Wall -Werror -Wshadow -pedantic -std=c++11 -I/usr/local/include -I../../../src -O3 -c testDecisionTree.cpp -o ../obj/opt/testDecisionTree.o
