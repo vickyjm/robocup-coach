7e62d461eea9408a26e82f5de12b7b631f4c337f
Jemd13 <jemd311@gmail.com>
2016-09-25 19:16:31 -0400

    Pushing the dectree.cpp

--- logs/opencv/dectree.cpp
+++ logs/opencv/dectree.cpp
@@ -76,7 +76,7 @@ int main(int argc, char* argv[]) {
     }
 
     int folds = 5;
-    int numSamples = 144035;
+    int numSamples = 144036;
     vector<int> trainingIndex;
     vector<int> testIndex;
 
@@ -91,19 +91,63 @@ int main(int argc, char* argv[]) {
 
     int* index = kfold(numSamples, folds);
 
+    // Testing related stuff :
+    vector<int>::iterator it; // Iterator for testIndex
+    CvDTreeNode* ans;        // Prediction obtained from the tree
+    int true0 = 0;           // Correctly classified 0s   
+    int true1 = 0;           // Correctly classified 1s
+    int false0 = 0;          // Incorrectly classified 0s
+    int false1 = 0;          // Incorrectly classified 1s
+    int totalTrue = 0;       // Total number of correctly classified actions
+    int totalFalse = 0;      // Total number of incorrectly classified actions
+
     for (int i = 0; i!=folds; i++){
         CvDTree dtree;
 
         trainingIndex = chooseIndex(index, 0, i, (numSamples*(folds-1))/folds, numSamples);
         testIndex = chooseIndex(index, 1, i, numSamples/folds, numSamples);
 
+        const Mat varIdx = Mat();
         const Mat training = Mat(trainingIndex);
 
         // Train the tree only with the training samples
-        dtree.train(values, CV_ROW_SAMPLE, responsesT, NULL, training);
+        dtree.train(values, CV_ROW_SAMPLE, responsesT, varIdx, training);
         
         //Probar
+        true0 = 0;           
+        true1 = 0;           
+        false0 = 0;          
+        false1 = 0;         
+        totalTrue = 0;       
+        totalFalse = 0;      
+        for (it=testIndex.begin() ; it < testIndex.end(); ++it) {
+            ans = dtree.predict(values.row(*it));
+
+            if (ans->value == responsesT.at<int>(0,*it)) {
+                if (ans->value == 0) {
+                    true0++; // The tree correctly predicted a 0 (unsuccessful action)
+                }
+                else {
+                    true1++; // The tree correctly predicted a 1 (successful action)
+                }
+            }
+            else if (ans->value != responsesT.at<int>(0,*it)) {
+                if ((ans->value == 0) && (responsesT.at<int>(0,*it) == 1)) {
+                    false0++; // The tree predicted a 0, but it was really a 1.
+                }
+                else {
+                    false1++; // The tree predicted a 1, but it was really a 0.
+                }
+            }
+        }
+
+        totalTrue = true0+true1;
+        totalFalse = false0+false1;
+
         //Sumar el error
+        std::cout << "Correctos : " << totalTrue << std::endl;
+        std::cout << "Incorrectos : " << totalFalse << std::endl;
+
     }
 
     /*
