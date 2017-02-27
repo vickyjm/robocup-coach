fac00a93a7ed7e477c764702d202bccbc5a86923
vickyjm <mvjorgemauriello@gmail.com>
2017-02-27 14:14:14 -0400

    Código de redes

--- logs/opencv/mlp.cpp
+++ logs/opencv/mlp.cpp
@@ -80,10 +80,12 @@ int main(int argc, char* argv[]) {
     int nHidden = atoi(argv[5]);
     int epochs = atoi(argv[6]);
     int features = atoi(argv[7]);
-   /* cout << "Total samples : " << numSamples << endl;
+    //int numTest = atoi(argv[8]);
+    cout << "Total samples : " << numSamples << endl;
     cout << "Folds : " << folds << endl;
     cout << "Using " << (numSamples*(folds-1))/folds << " samples to train" << endl;
-    cout << "and " << numSamples/folds << " samples to test" << endl;*/
+    cout << "and " << numSamples/folds << " samples to test" << endl;
+    //cout << "Validating with " << numTest << endl;
 
     vector<int> trainingIndex;
     vector<int> testIndex;
@@ -96,11 +98,11 @@ int main(int argc, char* argv[]) {
     const Mat values = aux(Range::all(), Range(0,features));
     const Mat responses(cvml.get_responses(),true);
 
-    int* index = kfold(numSamples, folds);
+    int* index = kfold(numSamples, folds); //Esto se comenta si no se usa cross-validation
 
     // Testing related stuff :
-    /*vector<int>::iterator
-     it; // Iterator for testIndex
+    vector<int>::iterator
+    it; // Iterator for testIndex
     int true0 = 0;           // Correctly classified 0s   
     int true1 = 0;           // Correctly classified 1s
     int false0 = 0;          // Incorrectly classified 0s
@@ -115,17 +117,16 @@ int main(int argc, char* argv[]) {
     int fn = 0;
     int finalTrue = 0;
     int finalFalse = 0;
-    float accuracy = 0;*/
+    float accuracy = 0;
     double predValue;
 
-    //std::cout << responses << std::endl;
+    //Este for se comenta para las redes finales, lo de adentro no
+   for (int i = 0; i!=folds; i++){
 
-   //for (int i = 0; i!=folds; i++){
+        trainingIndex = chooseIndex(index, 0, 0, (numSamples*(folds-1))/folds, numSamples); // Solo cuando usamos cross-validation
+        testIndex = chooseIndex(index, 1, i, numSamples/folds, numSamples);                 // Solo cuando usamos cross-validation
 
-        trainingIndex = chooseIndex(index, 0, 0, (numSamples*(folds-1))/folds, numSamples);
-        //testIndex = chooseIndex(index, 1, i, numSamples/folds, numSamples);
-
-        const Mat training = Mat(trainingIndex);
+        const Mat training = Mat(trainingIndex);          // Solo cuando usamos cross-validation
 
         cv::Mat layers = cv::Mat(3, 1, CV_32SC1);
 
@@ -151,32 +152,30 @@ int main(int argc, char* argv[]) {
         mlp.create(layers,CvANN_MLP::SIGMOID_SYM, 0.6,1);
 
         // train
-        mlp.train(values, responses, cv::Mat(), training, params);
+        mlp.train(values, responses, cv::Mat(), training, params);      // Esta se usa cuando estamos usando cross-validation
+        //mlp.train(values, responses, cv::Mat(), cv::Mat(), params);   // Esta se usa cuando queremos generar la red final para probar los de control
         
         
-        /*//Probar
+        //Probar
         true0 = 0;           
         true1 = 0;           
         false0 = 0;          
         false1 = 0;         
         totalTrue = 0;       
-        totalFalse = 0;    
+        totalFalse = 0;   
 
+        // Este ciclo debe comentarse cuando se quieren generar las redes finales
         for (it=testIndex.begin() ; it < testIndex.end(); ++it) {
             cv::Mat predAux(1, 1, CV_32FC1);
             cv::Mat predicted(numSamples/folds, 1, CV_32F);
-
             mlp.predict(values.row(*it), predAux);
             //predicted.at<float>(i,0) = predAux.at<float>(0,0);
-
             //std::cout << predAux.at<float>(0,0) << std::endl;
-
             if (predAux.at<float>(0,0) >= 0){
                 predValue = 1.0;
             } else {
                 predValue = -1.0;
             }
-
             if (predValue == responses.at<float>(*it,0)) {
                 if (predValue == -1.0) {
                     true0++; // The tree correctly predicted a 0 (unsuccessful action)
@@ -193,9 +192,10 @@ int main(int argc, char* argv[]) {
                     false1++; // The tree predicted a 1, but it was really a 0.
                 }
             }
-        }*/
+        }
 
-      /*  totalTrue = true0+true1;
+
+        totalTrue = true0+true1;
         totalFalse = false0+false1;
 
         tp = tp + true1;
@@ -203,11 +203,11 @@ int main(int argc, char* argv[]) {
         fp = fp + false1;
         fn = fn + false0;
         finalTrue = finalTrue + totalTrue;
-        finalFalse = finalFalse + totalFalse;*/
+        finalFalse = finalFalse + totalFalse;
         
-    //}
+    }
 
-    /*cout << "----------*-------------" << endl; 
+    cout << "----------*-------------" << endl; 
     cout << "True positives : " << tp  << endl;
     cout << "True negatives : " << tn  << endl;
 
@@ -215,10 +215,10 @@ int main(int argc, char* argv[]) {
     cout << "False negatives : " << fn  << endl;
 
     //Sumar el error
-    cout << "Total true : " << finalTrue << " - " << ((float)(finalTrue*100)/(float)(numSamples)) << '%' << endl;
-    cout << "Total false : " << finalFalse << " - " << ((float)(finalFalse*100)/(float)(numSamples)) << '%' << endl;*/
+    cout << "Total true : " << finalTrue << " - " << ((float)((finalTrue)*100)/(float)(numSamples)) << '%' << endl;
+    cout << "Total false : " << finalFalse << " - " << ((float)((finalFalse)*100)/(float)(numSamples)) << '%' << endl;
 
-    mlp.save(outFile);
+    //mlp.save(outFile);  // Descomentar este save cuando se quieren generar las redes finales
     free(outFile);
 
     return 0;
