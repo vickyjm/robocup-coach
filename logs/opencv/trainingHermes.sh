1bc858b976c042c5507517c43cb6e1a384c79d6e
vickyjm <mvjorgemauriello@gmail.com>
2017-03-07 08:47:19 -0400

    Scripts arreglados

--- logs/opencv/trainingHermes.sh
+++ logs/opencv/trainingHermes.sh
@@ -97,166 +97,166 @@ echo "============================" >> HermesSigmoidal.txt
 echo "============================" >> HermesGaussiana.txt
 
 echo "pass  10-200: " >> HermesSigmoidal.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N1 $E1 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N1 $E1 $FEAT 1 >> HermesSigmoidal.txt
 mv passMLP.yml ../Hermes/MLP/10-200/passSigmoidal.yml
 
 echo "pass  10-500: " >> HermesSigmoidal.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N1 $E2 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N1 $E2 $FEAT 1 >> HermesSigmoidal.txt
 mv passMLP.yml ../Hermes/MLP/10-500/passSigmoidal.yml
 
 echo "pass  20-200: " >> HermesSigmoidal.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N2 $E1 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N2 $E1 $FEAT 1 >> HermesSigmoidal.txt
 mv passMLP.yml ../Hermes/MLP/20-200/passSigmoidal.yml
 
 echo "pass  20-500: " >> HermesSigmoidal.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N2 $E2 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N2 $E2 $FEAT 1 >> HermesSigmoidal.txt
 mv passMLP.yml ../Hermes/MLP/20-500/passSigmoidal.yml
 
 echo "pass  30-200: " >> HermesSigmoidal.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N3 $E1 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N3 $E1 $FEAT 1 >> HermesSigmoidal.txt
 mv passMLP.yml ../Hermes/MLP/30-200/passSigmoidal.yml
 
 echo "pass  30-500: " >> HermesSigmoidal.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N3 $E2 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N3 $E2 $FEAT 1 >> HermesSigmoidal.txt
 mv passMLP.yml ../Hermes/MLP/30-500/passSigmoidal.yml
 
 echo "pass  50-500: " >> HermesSigmoidal.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N4 $E2 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N4 $E2 $FEAT 1 >> HermesSigmoidal.txt
 mv passMLP.yml ../Hermes/MLP/50-500/passSigmoidal.yml
 
 echo "pass  50-800: " >> HermesSigmoidal.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N4 $E3 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N4 $E3 $FEAT 1 >> HermesSigmoidal.txt
 mv passMLP.yml ../Hermes/MLP/50-800/passSigmoidal.yml
 
 echo "pass  80-500: " >> HermesSigmoidal.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N5 $E2 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N5 $E2 $FEAT 1 >> HermesSigmoidal.txt
 mv passMLP.yml ../Hermes/MLP/80-500/passSigmoidal.yml
 
 echo "pass  80-800: " >> HermesSigmoidal.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N5 $E3 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N5 $E3 $FEAT 1 >> HermesSigmoidal.txt
 mv passMLP.yml ../Hermes/MLP/80-800/passSigmoidal.yml
 
 
 echo "pass  10-200: " >> HermesGaussiana.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N1 $E1 $FEAT 0 >> HermesGaussiana.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N1 $E1 $FEAT 0 >> HermesGaussiana.txt
 mv passMLP.yml ../Hermes/MLP/10-200/passGaussian.yml
 
 echo "pass  10-500: " >> HermesGaussiana.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N1 $E2 $FEAT 0 >> HermesGaussiana.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N1 $E2 $FEAT 0 >> HermesGaussiana.txt
 mv passMLP.yml ../Hermes/MLP/10-500/passGaussian.yml
 
 echo "pass  20-200: " >> HermesGaussiana.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N2 $E1 $FEAT 0 >> HermesGaussiana.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N2 $E1 $FEAT 0 >> HermesGaussiana.txt
 mv passMLP.yml ../Hermes/MLP/20-200/passGaussian.yml
 
 echo "pass  20-500: " >> HermesGaussiana.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N2 $E2 $FEAT 0 >> HermesGaussiana.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N2 $E2 $FEAT 0 >> HermesGaussiana.txt
 mv passMLP.yml ../Hermes/MLP/20-500/passGaussian.yml
 
 echo "pass  30-200: " >> HermesGaussiana.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N3 $E1 $FEAT 0 >> HermesGaussiana.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N3 $E1 $FEAT 0 >> HermesGaussiana.txt
 mv passMLP.yml ../Hermes/MLP/30-200/passGaussian.yml
 
 echo "pass  30-500: " >> HermesGaussiana.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N3 $E2 $FEAT 0 >> HermesGaussiana.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N3 $E2 $FEAT 0 >> HermesGaussiana.txt
 mv passMLP.yml ../Hermes/MLP/30-500/passGaussian.yml
 
 echo "pass  50-500: " >> HermesGaussiana.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N4 $E2 $FEAT 0 >> HermesGaussiana.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N4 $E2 $FEAT 0 >> HermesGaussiana.txt
 mv passMLP.yml ../Hermes/MLP/50-500/passGaussian.yml
 
 echo "pass  50-800: " >> HermesGaussiana.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N4 $E3 $FEAT 0 >> HermesGaussiana.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N4 $E3 $FEAT 0 >> HermesGaussiana.txt
 mv passMLP.yml ../Hermes/MLP/50-800/passGaussian.yml
 
 echo "pass  80-500: " >> HermesGaussiana.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N5 $E2 $FEAT 0 >> HermesGaussiana.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N5 $E2 $FEAT 0 >> HermesGaussiana.txt
 mv passMLP.yml ../Hermes/MLP/80-500/passGaussian.yml
 
 echo "pass  80-800: " >> HermesGaussiana.txt
-./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N5 $E3 $FEAT 0 >> HermesGaussiana.txt
+./mlp -p ../Hermes/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N5 $E3 $FEAT 0 >> HermesGaussiana.txt
 mv passMLP.yml ../Hermes/MLP/80-800/passGaussian.yml
 
 echo "============================" >> HermesSigmoidal.txt
 echo "============================" >> HermesGaussiana.txt
 echo "shot  10-200: " >> HermesSigmoidal.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N1 $E1 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N1 $E1 $FEAT 1 >> HermesSigmoidal.txt
 mv shotMLP.yml ../Hermes/MLP/10-200/shotSigmoidal.yml
 
 echo "shot  10-500: " >> HermesSigmoidal.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N1 $E2 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N1 $E2 $FEAT 1 >> HermesSigmoidal.txt
 mv shotMLP.yml ../Hermes/MLP/10-500/shotSigmoidal.yml
 
 echo "shot  20-200: " >> HermesSigmoidal.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N2 $E1 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N2 $E1 $FEAT 1 >> HermesSigmoidal.txt
 mv shotMLP.yml ../Hermes/MLP/20-200/shotSigmoidal.yml
 
 echo "shot  20-500: " >> HermesSigmoidal.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N2 $E2 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N2 $E2 $FEAT 1 >> HermesSigmoidal.txt
 mv shotMLP.yml ../Hermes/MLP/20-500/shotSigmoidal.yml
 
 echo "shot  30-200: " >> HermesSigmoidal.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N3 $E1 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N3 $E1 $FEAT 1 >> HermesSigmoidal.txt
 mv shotMLP.yml ../Hermes/MLP/30-200/shotSigmoidal.yml
 
 echo "shot  30-500: " >> HermesSigmoidal.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N3 $E2 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N3 $E2 $FEAT 1 >> HermesSigmoidal.txt
 mv shotMLP.yml ../Hermes/MLP/30-500/shotSigmoidal.yml
 
 echo "shot  50-500: " >> HermesSigmoidal.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N4 $E2 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N4 $E2 $FEAT 1 >> HermesSigmoidal.txt
 mv shotMLP.yml ../Hermes/MLP/50-500/shotSigmoidal.yml
 
 echo "shot  50-800: " >> HermesSigmoidal.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N4 $E3 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N4 $E3 $FEAT 1 >> HermesSigmoidal.txt
 mv shotMLP.yml ../Hermes/MLP/50-800/shotSigmoidal.yml
 
 echo "shot  80-500: " >> HermesSigmoidal.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N5 $E2 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N5 $E2 $FEAT 1 >> HermesSigmoidal.txt
 mv shotMLP.yml ../Hermes/MLP/80-500/shotSigmoidal.yml
 
 echo "shot  80-800: " >> HermesSigmoidal.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N5 $E3 $FEAT 1 >> HermesSigmoidal.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N5 $E3 $FEAT 1 >> HermesSigmoidal.txt
 mv shotMLP.yml ../Hermes/MLP/80-800/shotSigmoidal.yml
 
 
 echo "shot  10-200: " >> HermesGaussiana.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N1 $E1 $FEAT 0 >> HermesGaussiana.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N1 $E1 $FEAT 0 >> HermesGaussiana.txt
 mv shotMLP.yml ../Hermes/MLP/10-200/shotGaussian.yml
 
 echo "shot  10-500: " >> HermesGaussiana.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N1 $E2 $FEAT 0 >> HermesGaussiana.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N1 $E2 $FEAT 0 >> HermesGaussiana.txt
 mv shotMLP.yml ../Hermes/MLP/10-500/shotGaussian.yml
 
 echo "shot  20-200: " >> HermesGaussiana.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N2 $E1 $FEAT 0 >> HermesGaussiana.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N2 $E1 $FEAT 0 >> HermesGaussiana.txt
 mv shotMLP.yml ../Hermes/MLP/20-200/shotGaussian.yml
 
 echo "shot  20-500: " >> HermesGaussiana.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N2 $E2 $FEAT 0 >> HermesGaussiana.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N2 $E2 $FEAT 0 >> HermesGaussiana.txt
 mv shotMLP.yml ../Hermes/MLP/20-500/shotGaussian.yml
 
 echo "shot  30-200: " >> HermesGaussiana.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N3 $E1 $FEAT 0 >> HermesGaussiana.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N3 $E1 $FEAT 0 >> HermesGaussiana.txt
 mv shotMLP.yml ../Hermes/MLP/30-200/shotGaussian.yml
 
 echo "shot  30-500: " >> HermesGaussiana.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N3 $E2 $FEAT 0 >> HermesGaussiana.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N3 $E2 $FEAT 0 >> HermesGaussiana.txt
 mv shotMLP.yml ../Hermes/MLP/30-500/shotGaussian.yml
 
 echo "shot  50-500: " >> HermesGaussiana.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N4 $E2 $FEAT 0 >> HermesGaussiana.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N4 $E2 $FEAT 0 >> HermesGaussiana.txt
 mv shotMLP.yml ../Hermes/MLP/50-500/shotGaussian.yml
 
 echo "shot  50-800: " >> HermesGaussiana.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N4 $E3 $FEAT 0 >> HermesGaussiana.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N4 $E3 $FEAT 0 >> HermesGaussiana.txt
 mv shotMLP.yml ../Hermes/MLP/50-800/shotGaussian.yml
 
 echo "shot  80-500: " >> HermesGaussiana.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N5 $E2 $FEAT 0 >> HermesGaussiana.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N5 $E2 $FEAT 0 >> HermesGaussiana.txt
 mv shotMLP.yml ../Hermes/MLP/80-500/shotGaussian.yml
 
 echo "shot  80-800: " >> HermesGaussiana.txt
-./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N5 $E3 $FEAT 0 >> HermesGaussiana.txt
+./mlp -s ../Hermes/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N5 $E3 $FEAT 0 >> HermesGaussiana.txt
 mv shotMLP.yml ../Hermes/MLP/80-800/shotGaussian.yml
 echo "==== " >> HermesSigmoidal.txt
--- logs/opencv/trainingJaeger.sh
+++ logs/opencv/trainingJaeger.sh
@@ -97,166 +97,166 @@ echo "============================" >> JaegerSigmoidal.txt
 echo "============================" >> JaegerGaussiana.txt
 
 echo "pass  10-200: " >> JaegerSigmoidal.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N1 $E1 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N1 $E1 $FEAT 1 >> JaegerSigmoidal.txt
 mv passMLP.yml ../Jaeger/MLP/10-200/passSigmoidal.yml
 
 echo "pass  10-500: " >> JaegerSigmoidal.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N1 $E2 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N1 $E2 $FEAT 1 >> JaegerSigmoidal.txt
 mv passMLP.yml ../Jaeger/MLP/10-500/passSigmoidal.yml
 
 echo "pass  20-200: " >> JaegerSigmoidal.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N2 $E1 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N2 $E1 $FEAT 1 >> JaegerSigmoidal.txt
 mv passMLP.yml ../Jaeger/MLP/20-200/passSigmoidal.yml
 
 echo "pass  20-500: " >> JaegerSigmoidal.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N2 $E2 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N2 $E2 $FEAT 1 >> JaegerSigmoidal.txt
 mv passMLP.yml ../Jaeger/MLP/20-500/passSigmoidal.yml
 
 echo "pass  30-200: " >> JaegerSigmoidal.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N3 $E1 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N3 $E1 $FEAT 1 >> JaegerSigmoidal.txt
 mv passMLP.yml ../Jaeger/MLP/30-200/passSigmoidal.yml
 
 echo "pass  30-500: " >> JaegerSigmoidal.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N3 $E2 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N3 $E2 $FEAT 1 >> JaegerSigmoidal.txt
 mv passMLP.yml ../Jaeger/MLP/30-500/passSigmoidal.yml
 
 echo "pass  50-500: " >> JaegerSigmoidal.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N4 $E2 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N4 $E2 $FEAT 1 >> JaegerSigmoidal.txt
 mv passMLP.yml ../Jaeger/MLP/50-500/passSigmoidal.yml
 
 echo "pass  50-800: " >> JaegerSigmoidal.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N4 $E3 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N4 $E3 $FEAT 1 >> JaegerSigmoidal.txt
 mv passMLP.yml ../Jaeger/MLP/50-800/passSigmoidal.yml
 
 echo "pass  80-500: " >> JaegerSigmoidal.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N5 $E2 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N5 $E2 $FEAT 1 >> JaegerSigmoidal.txt
 mv passMLP.yml ../Jaeger/MLP/80-500/passSigmoidal.yml
 
 echo "pass  80-800: " >> JaegerSigmoidal.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N5 $E3 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N5 $E3 $FEAT 1 >> JaegerSigmoidal.txt
 mv passMLP.yml ../Jaeger/MLP/80-800/passSigmoidal.yml
 
 
 echo "pass  10-200: " >> JaegerGaussiana.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N1 $E1 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N1 $E1 $FEAT 0 >> JaegerGaussiana.txt
 mv passMLP.yml ../Jaeger/MLP/10-200/passGaussian.yml
 
 echo "pass  10-500: " >> JaegerGaussiana.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N1 $E2 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N1 $E2 $FEAT 0 >> JaegerGaussiana.txt
 mv passMLP.yml ../Jaeger/MLP/10-500/passGaussian.yml
 
 echo "pass  20-200: " >> JaegerGaussiana.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N2 $E1 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N2 $E1 $FEAT 0 >> JaegerGaussiana.txt
 mv passMLP.yml ../Jaeger/MLP/20-200/passGaussian.yml
 
 echo "pass  20-500: " >> JaegerGaussiana.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N2 $E2 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N2 $E2 $FEAT 0 >> JaegerGaussiana.txt
 mv passMLP.yml ../Jaeger/MLP/20-500/passGaussian.yml
 
 echo "pass  30-200: " >> JaegerGaussiana.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N3 $E1 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N3 $E1 $FEAT 0 >> JaegerGaussiana.txt
 mv passMLP.yml ../Jaeger/MLP/30-200/passGaussian.yml
 
 echo "pass  30-500: " >> JaegerGaussiana.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N3 $E2 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N3 $E2 $FEAT 0 >> JaegerGaussiana.txt
 mv passMLP.yml ../Jaeger/MLP/30-500/passGaussian.yml
 
 echo "pass  50-500: " >> JaegerGaussiana.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N4 $E2 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N4 $E2 $FEAT 0 >> JaegerGaussiana.txt
 mv passMLP.yml ../Jaeger/MLP/50-500/passGaussian.yml
 
 echo "pass  50-800: " >> JaegerGaussiana.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N4 $E3 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N4 $E3 $FEAT 0 >> JaegerGaussiana.txt
 mv passMLP.yml ../Jaeger/MLP/50-800/passGaussian.yml
 
 echo "pass  80-500: " >> JaegerGaussiana.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N5 $E2 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N5 $E2 $FEAT 0 >> JaegerGaussiana.txt
 mv passMLP.yml ../Jaeger/MLP/80-500/passGaussian.yml
 
 echo "pass  80-800: " >> JaegerGaussiana.txt
-./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $D $N5 $E3 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -p ../Jaeger/dats/OnlyJEMV/Normalizados/passFile.dat 5 $P $N5 $E3 $FEAT 0 >> JaegerGaussiana.txt
 mv passMLP.yml ../Jaeger/MLP/80-800/passGaussian.yml
 
 echo "============================" >> JaegerSigmoidal.txt
 echo "============================" >> JaegerGaussiana.txt
 echo "shot  10-200: " >> JaegerSigmoidal.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N1 $E1 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N1 $E1 $FEAT 1 >> JaegerSigmoidal.txt
 mv shotMLP.yml ../Jaeger/MLP/10-200/shotSigmoidal.yml
 
 echo "shot  10-500: " >> JaegerSigmoidal.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N1 $E2 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N1 $E2 $FEAT 1 >> JaegerSigmoidal.txt
 mv shotMLP.yml ../Jaeger/MLP/10-500/shotSigmoidal.yml
 
 echo "shot  20-200: " >> JaegerSigmoidal.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N2 $E1 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N2 $E1 $FEAT 1 >> JaegerSigmoidal.txt
 mv shotMLP.yml ../Jaeger/MLP/20-200/shotSigmoidal.yml
 
 echo "shot  20-500: " >> JaegerSigmoidal.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N2 $E2 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N2 $E2 $FEAT 1 >> JaegerSigmoidal.txt
 mv shotMLP.yml ../Jaeger/MLP/20-500/shotSigmoidal.yml
 
 echo "shot  30-200: " >> JaegerSigmoidal.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N3 $E1 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N3 $E1 $FEAT 1 >> JaegerSigmoidal.txt
 mv shotMLP.yml ../Jaeger/MLP/30-200/shotSigmoidal.yml
 
 echo "shot  30-500: " >> JaegerSigmoidal.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N3 $E2 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N3 $E2 $FEAT 1 >> JaegerSigmoidal.txt
 mv shotMLP.yml ../Jaeger/MLP/30-500/shotSigmoidal.yml
 
 echo "shot  50-500: " >> JaegerSigmoidal.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N4 $E2 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N4 $E2 $FEAT 1 >> JaegerSigmoidal.txt
 mv shotMLP.yml ../Jaeger/MLP/50-500/shotSigmoidal.yml
 
 echo "shot  50-800: " >> JaegerSigmoidal.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N4 $E3 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N4 $E3 $FEAT 1 >> JaegerSigmoidal.txt
 mv shotMLP.yml ../Jaeger/MLP/50-800/shotSigmoidal.yml
 
 echo "shot  80-500: " >> JaegerSigmoidal.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N5 $E2 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N5 $E2 $FEAT 1 >> JaegerSigmoidal.txt
 mv shotMLP.yml ../Jaeger/MLP/80-500/shotSigmoidal.yml
 
 echo "shot  80-800: " >> JaegerSigmoidal.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N5 $E3 $FEAT 1 >> JaegerSigmoidal.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N5 $E3 $FEAT 1 >> JaegerSigmoidal.txt
 mv shotMLP.yml ../Jaeger/MLP/80-800/shotSigmoidal.yml
 
 
 echo "shot  10-200: " >> JaegerGaussiana.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N1 $E1 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N1 $E1 $FEAT 0 >> JaegerGaussiana.txt
 mv shotMLP.yml ../Jaeger/MLP/10-200/shotGaussian.yml
 
 echo "shot  10-500: " >> JaegerGaussiana.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N1 $E2 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N1 $E2 $FEAT 0 >> JaegerGaussiana.txt
 mv shotMLP.yml ../Jaeger/MLP/10-500/shotGaussian.yml
 
 echo "shot  20-200: " >> JaegerGaussiana.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N2 $E1 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N2 $E1 $FEAT 0 >> JaegerGaussiana.txt
 mv shotMLP.yml ../Jaeger/MLP/20-200/shotGaussian.yml
 
 echo "shot  20-500: " >> JaegerGaussiana.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N2 $E2 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N2 $E2 $FEAT 0 >> JaegerGaussiana.txt
 mv shotMLP.yml ../Jaeger/MLP/20-500/shotGaussian.yml
 
 echo "shot  30-200: " >> JaegerGaussiana.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N3 $E1 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N3 $E1 $FEAT 0 >> JaegerGaussiana.txt
 mv shotMLP.yml ../Jaeger/MLP/30-200/shotGaussian.yml
 
 echo "shot  30-500: " >> JaegerGaussiana.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N3 $E2 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N3 $E2 $FEAT 0 >> JaegerGaussiana.txt
 mv shotMLP.yml ../Jaeger/MLP/30-500/shotGaussian.yml
 
 echo "shot  50-500: " >> JaegerGaussiana.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N4 $E2 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N4 $E2 $FEAT 0 >> JaegerGaussiana.txt
 mv shotMLP.yml ../Jaeger/MLP/50-500/shotGaussian.yml
 
 echo "shot  50-800: " >> JaegerGaussiana.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N4 $E3 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N4 $E3 $FEAT 0 >> JaegerGaussiana.txt
 mv shotMLP.yml ../Jaeger/MLP/50-800/shotGaussian.yml
 
 echo "shot  80-500: " >> JaegerGaussiana.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N5 $E2 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N5 $E2 $FEAT 0 >> JaegerGaussiana.txt
 mv shotMLP.yml ../Jaeger/MLP/80-500/shotGaussian.yml
 
 echo "shot  80-800: " >> JaegerGaussiana.txt
-./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $D $N5 $E3 $FEAT 0 >> JaegerGaussiana.txt
+./mlp -s ../Jaeger/dats/OnlyJEMV/Normalizados/shotFile.dat 5 $T $N5 $E3 $FEAT 0 >> JaegerGaussiana.txt
 mv shotMLP.yml ../Jaeger/MLP/80-800/shotGaussian.yml
 echo "==== " >> JaegerSigmoidal.txt
